9dde26d80e0fec0483cbb238b727f5d1bf0b8dd3
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-04-28 16:27:00 +0900

    add weather emoji

--- weather.py
+++ weather.py
@@ -1,6 +1,10 @@
 from urllib import request
 import json
 
+def get_emoji(code):
+    emojis = {1: '☀', 2: '☁', 3: '☂', 4: '☃'}
+    return emojis.get(int(code) // 100)
+
 def fetch_weather():
     path_code = "130000" # tokyo
     area_detail_code = "130010" # tokyo
@@ -11,7 +15,7 @@ def fetch_weather():
        result = ''
        for area in content[0].get('timeSeries')[0].get('areas'):
            if area.get('area').get('code') == area_detail_code:
-               for time_define, weather in zip(content[0].get('timeSeries')[0].get('timeDefines'), area.get('weathers')):
-                   result +=f"{time_define}: {weather}\n"
+               for time_define, weather, weather_code in zip(content[0].get('timeSeries')[0].get('timeDefines'), area.get('weathers'), area.get('weatherCodes')):
+                   result +=f"{time_define}: {get_emoji(weather_code)} {weather}\n"
        return f"東京の天気\n{result}"
     return ''
