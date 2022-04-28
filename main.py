3c678eb8988b0a511efd74cda89dab67047e9d5d
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-04-28 16:41:16 +0900

    change discordのコマンドフォーマットに沿うようにした

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ async def on_message(message):
 
     channel = client.get_channel(CHANNELID)
 
-    if message.content.startswith('!meigen'):
+    if message.content.startswith('/meigen'):
         response_message = fetch_fortune()
         await channel.send(response_message)
         print(response_message)
     
-    if message.content.startswith('!weather'):
+    if message.content.startswith('/weather'):
         response_message = fetch_weather()
         await channel.send(response_message)
         print(response_message)
