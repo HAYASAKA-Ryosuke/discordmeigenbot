2f06d6bb180b1d126193b15bc425329ecb49c04b
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-06-01 17:01:34 +0900

    コロンがぬけていたので修正

--- erase_query_parameter.py
+++ erase_query_parameter.py
@@ -1,5 +1,5 @@
-import urllib
+from urllib.parse import urlparse
 
 def erase_query_parameter(url: str):
-    parsing_url = urllib.parse.urlparse(url)
-    return f'[here]({parsing_url.scheme}//{parsing_url.hostname}{parsing_url.path})'
+    parsing_url = urlparse(url)
+    return f'[here]({parsing_url.scheme}://{parsing_url.hostname}{parsing_url.path})'
