d21527fdedfc33a18cbfc9265b89f8ad7b65c155
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-06-01 17:07:44 +0900

    urlというパラメータはないのでvalueに変更

--- erase_query_parameter.py
+++ erase_query_parameter.py
@@ -5,5 +5,5 @@ def erase_query_parameter(url: str):
     embed = discord.Embed()
     parsing_url = urlparse(url)
     erased_query_param_url = f'{parsing_url.scheme}://{parsing_url.hostname}{parsing_url.path}'
-    embed.add_field(name=erased_query_param_url, url=erased_query_param_url)
+    embed.add_field(name=erased_query_param_url, value=erased_query_param_url)
     return embed
